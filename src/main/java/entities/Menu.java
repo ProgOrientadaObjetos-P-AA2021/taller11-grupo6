335576dc19bfc5ca6918d208d340cb1644a5b4ac
bitxanax <bitxanax@gmail.com>
2021-06-24 15:43:58 -0500

    m

---
+++ src/main/java/entities/Menu.java
@@ -0,0 +1,31 @@
+package entities;
+
+public abstract class Menu {
+    String nombrePlato;
+    double valorMenu;
+    double valorInicial;
+    
+    public String getNombrePlato() {
+        return nombrePlato;
+    }
+
+    public void setNombrePlato(String nombrePlato) {
+        this.nombrePlato = nombrePlato;
+    }
+
+    public double getValorMenu() {
+        return valorMenu;
+    }
+
+    public void setValorMenu(double valorMenu) {
+        this.valorMenu = valorMenu;
+    }
+
+    public double getValorInicial() {
+        return valorInicial;
+    }
+
+    public void setValorInicial(double valorInicial) {
+        this.valorInicial = valorInicial;
+    }
+}
