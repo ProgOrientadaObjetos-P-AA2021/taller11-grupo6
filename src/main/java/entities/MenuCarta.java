f2a6c5a42f5a8b44f74f5002793a1beadf9f0d84
¨Gerald¨ <geraldjt.gmail.com>
2021-06-24 16:20:57 -0500

    t

---
+++ src/main/java/entities/MenuCarta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author USUARIO PC
+ */
+public class MenuCarta extends Menu{
+    
+    
+    public MenuCarta(String nombrePlato, double valorMenu, double valorInicial) {
+        super(nombrePlato, valorMenu, valorInicial);
+    }
+    
+    
+}
